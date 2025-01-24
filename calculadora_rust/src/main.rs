7f1300bc3e0d63d8f22ea14a5a641bb70d642358
Oscar Riojas <oscarriojas@gmail.com>
2025-01-23 21:38:13 -0500

    feat/ se incluye funcion para ingresar valores desde terminal

--- calculadora_rust/src/main.rs
+++ calculadora_rust/src/main.rs
@@ -1,14 +1,56 @@
+// fn main() {
+//     let a = 5;
+//     let b = 6;
+//     let c = sumar(a, b);
+//     let d = restar(a, b);
+//     let e = multiplicar(a, b);
+//     let f = dividir(a, b);
+//     println!("Suma: {}", c);
+//     println!("Resta: {}", d);
+//     println!("Multiplicacion: {}", e);
+//     println!("Division: {}", f);
+// }
+
+// fn sumar(a: i32, b: i32) -> i32 {
+//     a + b
+// }
+
+// fn restar(a: i32, b: i32) -> i32 {
+//     a - b
+// }
+
+// fn multiplicar(a: i32, b: i32) -> i32 {
+//     a * b
+// }
+
+// fn dividir(a: i32, b: i32) -> f64 {
+//     (a as f64) / (b as f64)
+// }
+
+use std::io;
+
 fn main() {
-    let a = 5;
-    let b = 6;
+    println!("Ingrese el valor de a:");
+    let a = leer_valor();
+    println!("Ingrese el valor de b:");
+    let b = leer_valor();
+
     let c = sumar(a, b);
     let d = restar(a, b);
     let e = multiplicar(a, b);
     let f = dividir(a, b);
+
     println!("Suma: {}", c);
     println!("Resta: {}", d);
     println!("Multiplicacion: {}", e);
     println!("Division: {}", f);
+}
+
+fn leer_valor() -> i32 {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Error leyendo el valor");
+    let valor: i32 = input.trim().parse().expect("Error convirtiendo el valor");
+    valor
 }
 
 fn sumar(a: i32, b: i32) -> i32 {
